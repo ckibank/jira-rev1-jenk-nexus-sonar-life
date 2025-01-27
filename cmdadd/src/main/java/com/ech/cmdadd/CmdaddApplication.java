48a6dfec43238fd2e8c98453eadb891b297e5eae
ckibank <80457638+ckibank@users.noreply.github.com>
2025-01-27 20:08:44 +0000

    small hellow world cmdline program

---
+++ cmdadd/src/main/java/com/ech/cmdadd/CmdaddApplication.java
@@ -0,0 +1,13 @@
+package com.ech.cmdadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdaddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmdaddApplication.class, args);
+	}
+
+}
