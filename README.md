addcad35324f47232eb5ec7ccd18e6e6e7a8cf83
ckibank <80457638+ckibank@users.noreply.github.com>
2025-01-27 14:24:36 +0000

    Created maven project, working locally

---
+++ README.md
@@ -0,0 +1,8 @@
+# Works space for Java - maven - sonarqube - nexus 
+
+- apt update
+- install
+  - import java project
+  - mvn
+  - start docker compose
+
---
+++ demo/src/test/java/com/echdevop/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echdevop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
